23a372f6fa7edbf060d88b3ac648939a1d4c3249
Domonique Griffin <griffind2009@gmail.com>
2016-10-26 13:42:52 -0400

    app is up

--- js/app.js
+++ js/app.js
@@ -1,5 +1,26 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+let wdData = [
+  {author: "a", photoUrl: "", body: "d"},
+  {author: "b", photoUrl: "", body: "e"},
+  {author: "c", photoUrl: "", body: "f"}
+]
+
+
+  angular
+  .module("wdinstagram", ["ui.router"])
+  .controller("wdCtrl", [ wdController])
+
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+
+
+function RouterFunction ($stateProvider) {
+console.log("Working")
+}
+
+function wdController () {
+  this.wd = wdData
+}
